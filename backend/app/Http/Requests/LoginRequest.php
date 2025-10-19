4b99e6e404f5cfa22f6d8ab228a9b6044fb7c474
dasayo1215 <yamada.sayo1215@gmail.com>
2025-10-19 12:45:29 +0400

    更新

--- backend/app/Http/Requests/LoginRequest.php
+++ backend/app/Http/Requests/LoginRequest.php
@@ -14,7 +14,7 @@ class LoginRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'email' => ['required', 'email', 'string', 'max:255'],
+            'email' => ['required', 'string', 'max:255'],
             'password' => ['required', 'string'],
         ];
     }
@@ -25,9 +25,6 @@ class LoginRequest extends FormRequest
             // 未入力
             'email.required' => 'メールアドレスを入力してください',
             'password.required' => 'パスワードを入力してください',
-
-            // 形式不正
-            'email.email' => '正しいメールアドレス形式で入力してください',
         ];
     }
 }
